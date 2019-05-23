274e6774153d69fbc03a49681135aa960cde2a42
Kaziux <pelenyt3@gmail.com>
2019-05-23 19:50:56 +0300

    commit

---
+++ project3/README.md
@@ -0,0 +1,5 @@
+# Project 3
+
+ Restaurant application made python(Django).
+
+Clients are able to register login and logout from application. When logged in clients can add different items to their cart. When clients comfirm their orders they are notified with a message in their email
---
+++ project3/orders/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Salad, Order, Pasta, Regular_Pizza, Topping, Sicilian_Pizza, Dinner_Platters, Subs
+
+
+admin.site.register(Salad)
+admin.site.register(Pasta)
+admin.site.register(Order)
+admin.site.register(Topping)
+admin.site.register(Regular_Pizza)
+admin.site.register(Sicilian_Pizza)
+admin.site.register(Dinner_Platters)
+admin.site.register(Subs)
+
+
